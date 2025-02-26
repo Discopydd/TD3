f79a59e9d2f3fd6f181ee6149f43d43a85406b01
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2025-02-26 11:17:44 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectXGame/scene/GameScene.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <KamataEngine.h>
+#include <sstream>
+
+
+using namespace KamataEngine;
+class Enemy;
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
+	Camera camera_;
+	Model* model_ = nullptr;
+
+	DebugCamera* debugCamera_ = nullptr;
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+};
